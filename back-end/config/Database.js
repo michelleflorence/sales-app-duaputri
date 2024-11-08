a242b03f1cc3f30154336395cf519803d026990d
michelleflorence <michelleflorence86@gmail.com>
2024-11-08 17:58:29 +0700

    Adjusting database.js (not sure if this works)

--- back-end/config/Database.js
+++ back-end/config/Database.js
@@ -1,16 +1,31 @@
 import { Sequelize } from "sequelize";
 import pg from "pg";
 import "dotenv/config";
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+  NODE_ENV = "production",
+} = process.env;
 
 // Inisialisasi database
-const db = new Sequelize(
-  "postgres://postgres:password@localhost:5432/duaputri",
-  {
-    host: "localhost",
-    dialect: "postgres",
-    dialectModule: pg,
-  }
-);
+const db =
+  NODE_ENV === "production"
+    ? new Sequelize({
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        database: DB_NAME,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        dialectModule: pg,
+      })
+    : new Sequelize("postgres://postgres:password@localhost:5432/duaputri", {
+        host: "localhost",
+        dialect: "postgres",
+        dialectModule: pg,
+      });
 
 db.authenticate()
   .then(() => console.log("Connection established successfully."))
