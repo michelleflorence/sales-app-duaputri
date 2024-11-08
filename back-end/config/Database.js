f5b9a51e0bc1d87ea4c934b7aaa56fe514f9f66e
michelleflorence <michelleflorence86@gmail.com>
2024-11-08 15:13:49 +0700

    Fix database for sekian kali

--- back-end/config/Database.js
+++ back-end/config/Database.js
@@ -3,11 +3,19 @@ import pg from "pg";
 
 // Inisialisasi database
 const db = new Sequelize("duaputri", "postgres", "password", {
-  host: "localhost",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "postgres",
-  dialectModule: pg,
-  port: 5432,
-  timezone: "Etc/GMT+3",
+  dialectModule: pg, // I've added this.
+  timezone: "Etc/GMT+3", // Because process.env.TZ generated an error maybe due to time format
+  define: {
+    charset: "utf8mb4",
+    collate: "utf8mb4_general_ci",
+  },
+  logging: false,
 });
 
 db.authenticate()
