b05a0728b6b98526a013e08153128a516a132f2d
michelleflorence <michelleflorence86@gmail.com>
2024-11-08 14:30:40 +0700

    Fix database

--- back-end/config/Database.js
+++ back-end/config/Database.js
@@ -1,11 +1,13 @@
 import { Sequelize } from "sequelize";
-import pg from "pg"
+import pg from "pg";
 
 // Inisialisasi database
-const db = new Sequelize('duaputri', 'postgres', 'password', {
-    host: 'localhost', // Alamat server database
-    dialect: 'postgres', // Jenis database yang digunakan (PostgreSQL dalam kasus ini)
-    dialectModule: pg,
+const db = new Sequelize("duaputri", "postgres", "password", {
+  host: process.env.DB_HOST || "localhost", // Default to localhost
+  dialect: "postgres",
+  dialectModule: pg,
+  port: process.env.DB_PORT || 5432, // Default to port 5432
+  timezone: "Etc/GMT+3",
 });
 
 export default db;
